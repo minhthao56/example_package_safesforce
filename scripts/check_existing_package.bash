31e7b17110e54371f2fbd3b89b1ff76d87d8fce4
Minh Thao <minhthao5648@gmail.com>
2023-07-28 08:41:45 +0700

    fix: set output is bool

--- scripts/check_existing_package.bash
+++ scripts/check_existing_package.bash
@@ -1,8 +1,6 @@
 packageList=$(sfdx force:package:list -v dev_org_v8 --json)
 if echo "$packageList" | jq -e '.result | if . == null then [] else . end | map(select(.Name == "usermgmt")) | length == 1' > /dev/null; then
-  echo "Package already exists"
-  export PACKAGE_EXISTS=true
+  echo true
 else
-  echo "Package does not exist"
-  export PACKAGE_EXISTS=false
+  echo false
 fi
