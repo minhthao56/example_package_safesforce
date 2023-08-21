17a6a5b13aeca99fbbe014d3bf7157bebe2c0946
Minh Thao <minhthao5648@gmail.com>
2023-08-21 17:52:25 +0700

    update new flow

--- scripts/check_existing_package.bash
+++ scripts/check_existing_package.bash
@@ -1,5 +1,5 @@
 packageList=$(sfdx force:package:list -v dev_org_v8 --json)
-if echo "$packageList" | jq -e '.result | if . == null then [] else . end | map(select(.Name == "payment")) | length == 1' > /dev/null; then
+if echo "$packageList" | jq -e '.result | if . == null then [] else . end | map(select(.Name == "usermgmt")) | length == 1' > /dev/null; then
   echo true
 else
   echo false
